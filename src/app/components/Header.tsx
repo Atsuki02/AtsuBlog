2839fc7bbbcd8bcaf7137049af64ec1f030295b4
Atsuki Kitada <canvanvam@gmail.com>
2023-12-27 14:48:57 -0800

    fix SSR error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import NavBar from "./NavBar";
 import ThemeSwitcher from "./ThemeSwitcher";
 import Link from "next/link";
 import NavMenu from "./NavMenu";
 import { greatVibes } from "../layout";
-import { useMediaQuery } from "../lib/useMediaQuery";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { usePathname, useRouter } from "next/navigation";
@@ -14,7 +12,6 @@ import { CurrentUser } from "../types";
 import { signOut, useSession } from "next-auth/react";
 
 function Header({ currentUser }: { currentUser: CurrentUser | null }) {
-  const matches = useMediaQuery("(min-width: 675px)");
   const path = usePathname();
   const { data: session, status } = useSession();
   const isAuthenticated = status === "authenticated";
@@ -30,7 +27,9 @@ function Header({ currentUser }: { currentUser: CurrentUser | null }) {
     <div className="sticky left-0 right-0 top-0 z-30 flex h-20 w-full justify-between bg-slate-600 p-6 shadow-xl">
       <div className="flex flex-row items-center">
         <NavBar />
-        {matches && <ThemeSwitcher />}
+        <div className="hidden sm:block">
+          <ThemeSwitcher />
+        </div>
       </div>
       <div className="flex items-center justify-center">
         <Link href="/">
@@ -54,11 +53,11 @@ function Header({ currentUser }: { currentUser: CurrentUser | null }) {
             </button>
           </>
         )}
-        {!isAuthenticated && path !== "/auth/signin" && matches && (
+        {!isAuthenticated && path !== "/auth/signin" && (
           <Link href="/auth/signin">
             <button
               type="button"
-              className="mr-4 inline-flex items-center gap-x-2 rounded-lg border border-gray-200  px-4 py-2.5 text-sm font-medium text-white shadow-sm disabled:pointer-events-none disabled:opacity-50 dark:border-white dark:text-white  dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              className="mr-4 hidden items-center  gap-x-2 rounded-lg border border-gray-200 px-4  py-2.5 text-sm font-medium text-white shadow-sm disabled:pointer-events-none disabled:opacity-50 dark:border-white dark:text-white dark:focus:outline-none  dark:focus:ring-1 dark:focus:ring-gray-600 sm:inline-flex"
             >
               Log In
             </button>
